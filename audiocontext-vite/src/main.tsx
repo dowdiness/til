4702ed1d95e14dc5db47343c18f9a23ff1d244fa
Ishimoto Koji <koji.ishimoto@gmail.com>
2025-01-23 11:39:37 +0900

    Add Profiler for performance improvement

--- audiocontext-vite/src/main.tsx
+++ audiocontext-vite/src/main.tsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
+import { StrictMode, Profiler, type ProfilerOnRenderCallback } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.css'
 import App from './App.tsx'
 
+const isDev = false
+
+const onRenderCallback: ProfilerOnRenderCallback = (
+  id,
+  phase,
+  actualDuration,
+  baseDuration,
+  startTime,
+  commitTime,
+) => {
+  if (isDev) {
+    console.log('Profiler ID:', id)
+    console.log('Phase:', phase)
+    console.log('Actual Duration:', actualDuration)
+    console.log('Base Duration:', baseDuration)
+    console.log('Start Time:', startTime)
+    console.log('Commit Time:', commitTime)
+  }
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Profiler id="App" onRender={onRenderCallback}>
+      <App />
+    </Profiler>,
   </StrictMode>,
 )
